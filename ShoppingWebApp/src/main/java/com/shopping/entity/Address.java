37bd2b4dc4a90ab1256963853b7db15de10ae509
madhav sharma <madhavsharma7498@gmail.com>
2020-08-22 17:14:45 +0530

    comment in address class

--- ShoppingWebApp/src/main/java/com/shopping/entity/Address.java
+++ ShoppingWebApp/src/main/java/com/shopping/entity/Address.java
@@ -7,6 +7,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+//Address class by Madhav
 @Entity
 @Table(name="tbl_address")
 public class Address {
