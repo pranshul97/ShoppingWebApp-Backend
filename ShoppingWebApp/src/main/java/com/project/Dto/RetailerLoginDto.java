33c2ef52fa8c752a4f291d3248b2db4130831295
Bhavya <BHAVYA.Kumar@lntinfotech.com>
2020-08-24 02:26:05 +0530

    commit part3

---
+++ ShoppingWebApp/src/main/java/com/project/Dto/RetailerLoginDto.java
@@ -0,0 +1,21 @@
+package com.project.Dto;
+
+public class RetailerLoginDto {
+	private String email;
+	private String passsword;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPasssword() {
+		return passsword;
+	}
+	public void setPasssword(String passsword) {
+		this.passsword = passsword;
+	}
+	
+
+}
