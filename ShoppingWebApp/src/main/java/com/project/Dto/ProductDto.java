64f5efea9374e10dedeb6c7f79e2f7aaac32a72b
Pranshul Tiwari <pranshul.tiwari97@gmail.com>
2020-08-24 23:04:54 +0530

    Created product dto.

---
+++ ShoppingWebApp/src/main/java/com/project/Dto/ProductDto.java
@@ -0,0 +1,43 @@
+package com.project.Dto;
+
+public class ProductDto {
+
+	private int productId;
+	private String name;
+	private int categoryId;
+	private String brandName;
+	private double price;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	
+	public String getBrandName() {
+		return brandName;
+	}
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
--- ShoppingWebApp/src/main/java/com/project/controller/FetchProductController.java
+++ ShoppingWebApp/src/main/java/com/project/controller/FetchProductController.java
@@ -1,5 +1,6 @@
 package com.project.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.Dto.ProductDto;
 import com.project.controller.FetchProductController.Log.Statustype;
 import com.project.entity.Product;
 import com.project.exception.ProductsException;
@@ -36,8 +38,18 @@ public class FetchProductController {
 		try {
 			List<Product> list=prodService.fetchByName(productName);
 			Log log=new Log();
-			log.setStatus(Statustype.SUCCESS);
-			log.setList(list);
+			log.setStatus(Statustype.SUCCESS); 
+			List<ProductDto> prodList=new ArrayList<ProductDto>();
+			for(Product li: list) {
+				ProductDto prod=new ProductDto();
+				prod.setProductId(li.getProductId());
+				prod.setName(li.getName());
+				prod.setBrandName(li.getBrandName());
+				prod.setPrice(li.getPrice());
+				prod.setCategoryId(li.getCategory().getCategoryId());
+				prodList.add(prod);
+			}
+			log.setList(prodList);
 			return log;
 		}
 		catch(ProductsException p) {
@@ -50,7 +62,7 @@ public class FetchProductController {
 	
 	public static class Log{
 		private Statustype status;
-		private List<Product> list;
+		private List<ProductDto> list;
 		public static enum Statustype{
 			SUCCESS,FAILURE;
 		}
@@ -60,10 +72,10 @@ public class FetchProductController {
 		public void setStatus(Statustype status) {
 			this.status = status;
 		}
-		public List<Product> getList() {
+		public List<ProductDto> getList() {
 			return list;
 		}
-		public void setList(List<Product> list) {
+		public void setList(List<ProductDto> list) {
 			this.list = list;
 		}
 		
