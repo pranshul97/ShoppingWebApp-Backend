32df15a18590779a4f553ac8df6518392cb2d04a
madhav sharma <madhavsharma7498@gmail.com>
2020-08-25 09:43:25 +0530

    Compare conponent almost done ...Just image work is pending

--- ShoppingWebApp/src/main/java/com/project/controller/CompareController.java
+++ ShoppingWebApp/src/main/java/com/project/controller/CompareController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.Dto.CompareDto;
+import com.project.controller.CompareController.Log.Statustype;
 import com.project.entity.Product;
 import com.project.service.CompareMadhavServiceImpl;
 
@@ -25,8 +26,11 @@ public class CompareController {
 	
 	@PostMapping("/compare")
 	//public List<Product> fetchProductDetails(@RequestBody CompareDto compareDto) {
-	public List<Product> fetchProductDetails(@RequestBody int[] arr) {
+	public Log fetchProductDetails(@RequestBody int[] arr) {
 		List<Product> proList = new ArrayList<>();
+		Log log=new Log();
+		log.setStatus(Statustype.SUCCESS);
+		
 		//int idArrayLength = compareDto.arrayListSize();
 		for(int i=0; i<arr.length ;i++) {
 			//Product proDetails = compareService.fetchProductForCompare(compareDto.fetchElementValue(i));
@@ -34,7 +38,28 @@ public class CompareController {
 			proList.add(proDetails);
 		}
 		//System.out.println(proDetails.getName() + " " + proDetails.getCategory() + " " + proDetails.getModel());
-		return proList;
+		log.setList(proList);
+		return log;
 	}
+	
+	public static class Log{
+		private Statustype status;
+		private List<Product> list;
+		public static enum Statustype{
+		SUCCESS,FAILURE;
+		}
+		public Statustype getStatus() {
+		return status;
+		}
+		public void setStatus(Statustype status) {
+		this.status = status;
+		}
+		public List<Product> getList() {
+		return list;
+		}
+		public void setList(List<Product> list) {
+		this.list = list;
+		}
 
+	}
 }
