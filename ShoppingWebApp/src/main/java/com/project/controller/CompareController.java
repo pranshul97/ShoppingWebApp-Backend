577ccc4f9612c578825bfbc14d751a3182b72524
madhav sharma <madhavsharma7498@gmail.com>
2020-08-27 16:09:31 +0530

    indentation changes in controller

--- ShoppingWebApp/src/main/java/com/project/controller/CompareController.java
+++ ShoppingWebApp/src/main/java/com/project/controller/CompareController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -122,7 +123,7 @@ public class CompareController {
 		
 	}
 	
-	@PostMapping("/admin")
+	@GetMapping("/admin")
 	public Log fetchProductRetailerCategory() {
 		List<Product> prodList = compareService.fetchAllProductforAdmin();
 		List<Retailers> retList = compareService.fetchAllRetailersforAdmin();
